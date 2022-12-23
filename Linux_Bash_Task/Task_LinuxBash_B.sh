c0b73f048d60a8bc8a499d12833101036d488402
Alex Shvets <kazzayoun@gmail.com>
2022-12-23 20:10:56 +0200

    Finish task_B

--- Linux_Bash_Task/Task_LinuxBash_B.sh
+++ Linux_Bash_Task/Task_LinuxBash_B.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 echo "1. From which ip were the most requests? "
+sleep 2
 awk '{print $1}' apache_logs.txt | sort | uniq -c | sort -r | awk '{print $2}' | head -1
 
 echo "2. What is the most requested page?"
-grep '+http' apache_logs.txt | awk '{print $NF}' | sort | uniq -c | sort -r | awk '{print $2}' | head -1
-
+sleep 2
+grep 'GET' apache_logs.txt | awk '{print $6,$7,$8}' | sort | uniq -c | sort -r | head -1 | awk '{print $2 $3 $4}'
 
 echo "3. How many requests were there from each ip?"
+sleep 2
 awk '{print $1}' apache_logs.txt | sort | uniq -c | sort -k1 -r | awk '{print "Sum requests: " $1, " = ", $2}'
 
 echo "4. What non-existent pages were clients referred to?"
-
+sleep 2
+grep 'error404' apache_logs.txt | awk '{print $7, $8}'
 
 echo "5. What time did site get the most requests?"
-
+sleep 2
+awk '{print $4 }' apache_logs.txt | sort | uniq -c | sort -r | awk '{print $2 }' | head
 
 echo "6. What search bots have accessed the site? (UA + IP)"
+sleep 2
+awk '{print $1, $NF}' apache_logs.txt | grep -E 'bot|bots' | grep -v 'robots' | sort -r | uniq 
+
 
 
