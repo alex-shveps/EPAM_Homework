5e5398a241082425119a7e498d3cf67177b2361a
Alex Shvets <kazzayoun@gmail.com>
2023-01-10 18:53:23 +0200

    Finish task_A

--- Linux_Bash_Task/Task_LinuxBash_A.sh
+++ Linux_Bash_Task/Task_LinuxBash_A.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+
 sudo apt-get update
 sudo apt-get install nmap
-sudo apt-get install 
-all()
+sudo apt-get install net-tools
+
+
+key_all()
 {
         echo "Dispays the IP addresse and symbolic names of all hosts in the current subnet:"
-        sleep 5
-        echo
-        
-        ;
+        inet_ip=`(ifconfig | grep broadcast | awk '{print $2}')`
+        sudo nmap -sn $inet_ip/20;
 }
-target()
+
+key_target()
 {
         echo "Displays a list of open system TCP ports:"
-        sleep 5
-
-        ;
+        sudo ss -tlpn;
 }
+
 if [[ "$1" == "--all" ]]; then
-        all
+        key_all 
 elif [[ "$1" == "--target" ]]; then
-        target
+        key_target
 else echo "'--all'  key displays the IP addresses and symbolic names of all hosts in the current subnet
 '--target' key displays a list of open system TCP ports"
 fi
